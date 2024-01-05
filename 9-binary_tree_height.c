56fcfc25a89ea575a20c2c5608bf6301e6da7e86
ohendarko <ohendarko@gmail.com>
2024-01-05 05:27:35 +0000

    submit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,13 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
+	int height;
 
 	if (tree == NULL)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	height = 1 + (left_height > right_height ? left_height : right_height);
+	return (height);
 }
