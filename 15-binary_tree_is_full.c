9a8e631edf1972f45e39243069d588dfee0b82c8
ohendarko <ohendarko@gmail.com>
2024-01-05 07:48:19 +0000

    submit

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if ((tree->left == NULL && tree->right == NULL) ||
 		(tree->left != NULL && tree->right != NULL))
-		return (1);
+		if ((binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right)) ||
+			(!binary_tree_is_full(tree->left) && !binary_tree_is_full(tree->right)))
+			return (1);
 
 	return (0);
 }
